f9ab5003f7af6ea6f4732d91e0426d7522edeecf
Nathalie Daoût <nat.dao3@gmail.com>
2022-10-17 21:27:49 +0200

    infinity scroll

--- script.js
+++ script.js
@@ -4,18 +4,35 @@ const loader = document.getElementById('loader');
 
 // Constantes API
 const key = "dtkyZAV5zRvH10hIQ0tZ89CEuQeXWfFaQpLXEkEUYko";
-const count = 10;
+const count = 30;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${key}&count=${count}`;
 
+let imagesLoaded = 0;
+let totalImages = 0;
+let ready = false;
+
+// Helper function to set attributes on DOM elements
 function setAttribute(element, attributes) {
     for(const key in attributes){
         element.setAttribute(key, attributes[key]);
     }
 }
 
+// check if all images were loaded
+function loadImages() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
 function displayPhotos() {
+    totalImages = photosTab.length;
+    imagesLoaded = 0;
     // Création de l'élément html et de ses attributs
     photosTab.forEach((photo) => {
+        loadImages();
         const item = document.createElement('a');
         setAttribute(item, {
             href: photo.links.html,
@@ -39,12 +56,19 @@ async function getPhotosFromUnsplash() {
     try{
         const response = await fetch(apiUrl);
         photosTab = await response.json();
-        console.log(photosTab[2]);
         displayPhotos();
     } catch(error) {
         // action if error...
     }
 }
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= window.document.body.offsetHeight - 1000 && ready) {
+        getPhotosFromUnsplash();
+        ready = false;
+    }
+});
+
 
 // On load
 getPhotosFromUnsplash();
